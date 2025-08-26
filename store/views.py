faa2301ed4d62259ae3a8f166e70b803c9c8c4c1
RandomSummer <sksofiqueefiaz2015@gmail.com>
2025-08-26 12:11:31 +0530

    routing store app to store webpage

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+    return render(request,'store/store.html', context)

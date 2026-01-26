325b25885c6bfaaf283c5f20043d31669e2d1154
RandomSummer <sksofiqueefiaz2015@gmail.com>
2026-01-26 23:45:24 +0530

    creating variations

--- carts/views.py
+++ carts/views.py
@@ -14,6 +14,11 @@ def _cart_id(request): # _ for private fuctions
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET['color']
+    size = request.GET['size']
+    return HttpResponse(color+ ' '+size)
+    exit()
+
     product = Product.objects.get(id=product_id) # get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
